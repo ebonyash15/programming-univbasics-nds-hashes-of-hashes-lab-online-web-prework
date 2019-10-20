ef23db9b90c466036ac0fba2f416e62cccde2387
Ebony Sharrock <ebonysharrock@gmail.com>
2019-10-20 21:10:39 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -26,20 +26,17 @@
 
 def naming_system
   naming_hash = {
-    PORTION_1 =>
-     {label: "Kingdom",
+     label: "Kingdom",
       sub_category: {
           label: "Phylum",
-      sub_category: {
-          label: "Class",
-      sub_category:
-    {label: "Order"}}}},
-  PORTION_3 =>
-    {label: "Family",
-    sub_category: {
-      label: "Genus"}},
-  PORTION_4 =>
-    {label: "Species",
-    sub_category: {}}}
+            sub_category: {
+              label: "Class",
+                sub_category: {
+                  label: "Order", {
+                    label: "Family",
+                      sub_category: {
+                        label: "Genus", {
+                          label: "Species",
+                            sub_category: {}}}}}}}}
     p naming_hash
 end
