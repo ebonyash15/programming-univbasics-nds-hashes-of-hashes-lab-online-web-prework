d114e638e8e76571fd53d5279fc62e8643b17403
Ebony Sharrock <ebonysharrock@gmail.com>
2019-10-20 21:21:01 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -25,17 +25,18 @@
 # }
 def naming_system
   naming_hash = {
-    label: "Kingdom",
-      sub_category: {
-        label: "Phylum",
-          sub_category: {
-            label: "Class",
-              sub_category: {
-                label: "Order", {
-                  label: "Family",
-                    sub_category: {
-                      label: "Genus", {
-                        label: "Species",
-                          sub_category: nil }}}}}}}
+    label: "Kingdom", sub_category: {
+        label: "Phylum", sub_category: {
+            label: "Class", sub_category: {
+                label: "Order", sub_category: {
+                  label: "Family", sub_category: {
+                      label: "Genus", sub_category: {
+                        label: "Species", sub_category: {} }
+                      }
+                 }
+            }
+        }
+    }
+ }
     p naming_hash
 end
