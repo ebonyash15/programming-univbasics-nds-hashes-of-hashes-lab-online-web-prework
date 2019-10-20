ca25ff10b810a2bf46837bf96077bd5a25fca84e
Ebony Sharrock <ebonysharrock@gmail.com>
2019-10-20 21:21:32 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -31,7 +31,7 @@ def naming_system
                 label: "Order", sub_category: {
                   label: "Family", sub_category: {
                       label: "Genus", sub_category: {
-                        label: "Species", sub_category: {} }
+                        label: "Species", sub_category: nil }
                       }
                  }
             }
